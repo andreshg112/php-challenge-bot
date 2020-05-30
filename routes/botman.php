797c19713cbcd2e144acbf21af1f54d757024d09
Andrés Herrera García <andreshg112@gmail.com>
2020-05-30 13:40:19 -0500

    it answers to convert command

--- routes/botman.php
+++ routes/botman.php
@@ -6,7 +6,13 @@ use App\Http\Controllers\BotManController;
 $botman = resolve('botman');
 
 $botman->hears('Hi', function ($bot) {
+    /** @var \BotMan\BotMan\BotMan $bot */
     $bot->reply('Hello!');
 });
 
 $botman->hears('Start conversation', BotManController::class . '@startConversation');
+
+$botman->hears('convert {amount} {from} to {to}', function ($bot, $amount, $from, $to) {
+    /** @var \BotMan\BotMan\BotMan $bot */
+    $bot->reply("from: {$from}, to: {$to}, amount: {$amount}");
+});
